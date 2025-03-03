cabe26a15907b037ef71adb8a48ae9af26250aa3
Nguyễn Bá Tuấn Anh <anh.nguyen16@codegym.vn>
2025-03-03 14:17:15 +0700

    Fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ function App() {
   return (
     <>
       <h1>Trang chủ</h1>
-      <NavLink to="/vite-react-deploy/register">Đăng ký</NavLink> |{' '}
-      <NavLink to="/vite-react-deploy/login">Đăng nhập</NavLink> |{' '}
-      <NavLink to="/vite-react-deploy/dashboard">Quản trị</NavLink>
+      <NavLink to="/register">Đăng ký</NavLink> |{' '}
+      <NavLink to="/login">Đăng nhập</NavLink> |{' '}
+      <NavLink to="/dashboard">Quản trị</NavLink>
       <div>
         <p>
-          <NavLink to="/vite-react-deploy/posts/1">
+          <NavLink to="/posts/1">
             React khai tử công cụ create-react-app
           </NavLink>
         </p>
         <p>
-          <NavLink to="/vite-react-deploy/posts/2">React 19 có gì mới?</NavLink>
+          <NavLink to="/posts/2">React 19 có gì mới?</NavLink>
         </p>
       </div>
     </>
