5aab2b9ac9176326895c7425039278b555189d03
Nguyễn Bá Tuấn Anh <anh.nguyen16@codegym.vn>
2025-03-03 14:00:21 +0700

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { NavLink } from 'react-router';
 
@@ -8,17 +5,17 @@ function App() {
   return (
     <>
       <h1>Trang chủ</h1>
-      <NavLink to="/register">Đăng ký</NavLink> |{' '}
-      <NavLink to="/login">Đăng nhập</NavLink> |{' '}
-      <NavLink to="/dashboard">Quản trị</NavLink>
+      <NavLink to="/vite-react-deploy/register">Đăng ký</NavLink> |{' '}
+      <NavLink to="/vite-react-deploy/login">Đăng nhập</NavLink> |{' '}
+      <NavLink to="/vite-react-deploy/dashboard">Quản trị</NavLink>
       <div>
         <p>
-          <NavLink to="/posts/1">
+          <NavLink to="/vite-react-deploy/posts/1">
             React khai tử công cụ create-react-app
           </NavLink>
         </p>
         <p>
-          <NavLink to="/posts/2">React 19 có gì mới?</NavLink>
+          <NavLink to="/vite-react-deploy/posts/2">React 19 có gì mới?</NavLink>
         </p>
       </div>
     </>
